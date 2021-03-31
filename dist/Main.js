ac91774f9b7d7a7e9b66506e2d82fe366fafd567
Island <islandhuynh@gmail.com>
2021-03-30 22:30:36 -0400

    initial commit

---
+++ dist/Main.js
@@ -0,0 +1,7 @@
+export class Main {
+    constructor() {
+        console.log('Constructed new Instance of the program');
+    }
+}
+new Main();
+//# sourceMappingURL=Main.js.map
---
+++ src/Main.ts
@@ -0,0 +1,8 @@
+export class Main {
+
+  public constructor() {
+    console.log('Constructed new Instance of the program');
+  }
+}
+
+new Main();
